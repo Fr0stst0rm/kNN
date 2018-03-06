d42fed5d0ad8c9ac7c4fa75e5d068cf89075d4ce
Christoph Cudly <christoph.cudly@gmail.com>
2018-03-06 14:49:52 +0100

    Created a thread for the algorithm

--- src/kNN/kNNAlgorithm.java
+++ src/kNN/kNNAlgorithm.java
@@ -1,5 +1,32 @@
 package kNN;
 
-public class kNNAlgorithm {
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class kNNAlgorithm implements Runnable {
+
+	private Thread th;
+	private boolean alive = true;
+
+	private Date start;
+
+	public kNNAlgorithm() {
+		th = new Thread(this);
+	}
+
+	void start() {
+		start = new Date();
+		th.start();
+	}
+
+	@Override
+	public void run() {
+		while (alive) {
+			alive = false;
+		}
+		LocalDateTime runTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(new Date().getTime() - start.getTime()), TimeZone.getDefault().toZoneId());
+		System.out.println("Time: " + runTime);
+	}
 
 }
